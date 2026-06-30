9f632d715928b4a2206f29366c3f2f7f05395544
Dev-Fiyin <clementfiyinfoluwa05@gmail.com>
2026-06-30 19:41:44 +0100

    Update API URL to production backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://barberpro-backend-nv11.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
